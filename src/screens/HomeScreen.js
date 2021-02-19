de9111ef8e70c8b073ec72b87cc970f69966b4ac
nguyenhung <hunghung0309@gmail.com>
2021-02-19 10:14:27 +0700

    28. Touchable Opacity in Action

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,22 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity} from "react-native";
+
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hello</Text>;
+  return (
+  <View>
+    <Text style={styles.text}>Hello</Text>
+    <Button 
+      onPress={() => console.log('Button pressed')}
+      title='Go to component demo' />
+      <TouchableOpacity onPress= {() => console.log('List Press')}>
+        <Text>Go to List Demo</Text>
+        <Text>Go to List Demo</Text>
+        <Text>Go to List Demo</Text>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+  </View>
+  )
 };
 
 const styles = StyleSheet.create({
