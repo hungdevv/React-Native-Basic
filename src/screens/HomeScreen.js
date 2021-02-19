2332915c5b89149413e6d16fe4a9cb205332ee48
nguyenhung <hunghung0309@gmail.com>
2021-02-19 10:46:42 +0700

    29. Navigating

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity} from "react-native";
 
 
-const HomeScreen = () => {
+const HomeScreen = props => {
   return (
   <View>
     <Text style={styles.text}>Hello</Text>
     <Button 
-      onPress={() => console.log('Button pressed')}
+      onPress={() => props.navigation.navigate('Components')}
       title='Go to component demo' />
       <TouchableOpacity onPress= {() => console.log('List Press')}>
         <Text>Go to List Demo</Text>
