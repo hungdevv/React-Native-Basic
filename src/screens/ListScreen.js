34b0a2a27659d0b7cef98fc7c30748e5513777a3
nguyenhung <hunghung0309@gmail.com>
2021-02-19 09:25:46 +0700

    20. rendering a FlatList

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, FlatList} from 'react-native';
 
 const ListScreen = () => {
-    return <Text>List Screen</Text>;
+    // return <Text>List Screen</Text>;
+    const friends = [
+        {name: 'Hung1'},
+        {name: 'Hung2'},
+        {name: 'Hung3'},
+        {name: 'Hung4'},
+        {name: 'Hung5'},
+        {name: 'Hung6'},
+        {name: 'Hung7'},
+
+    ];
+    return (
+    <FlatList 
+        data={friends} 
+        renderItem={({item}) => {
+            return <Text>{item.name}</Text>
+        }}
+        />
+    );
 };
 
 const styles = StyleSheet.create({});
