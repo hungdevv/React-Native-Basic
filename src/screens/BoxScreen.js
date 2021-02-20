2ca305cb5379dbc56ed5d32459db06667ec4e8c9
nguyenhung <hunghung0309@gmail.com>
2021-02-20 16:19:51 +0700

    Exercise Solution

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -5,9 +5,9 @@ const BoxScreen = () => {
 
     return (
         <View style={styles.viewStyle}>
-            <Text style={styles.textOneStyle}>Go to BoxScreen1</Text>
-            <Text style={styles.textTwoStyle}>Go to BoxScreen2</Text>
-            <Text style={styles.textThreeStyle}>Go to BoxScreen3</Text>
+            <View style={styles.boxOneStyle} />
+            <View style={styles.boxTwoStyle}/>
+            <View style={styles.boxThreeStyle}/>
         </View>
     )
 }
@@ -15,42 +15,31 @@ const BoxScreen = () => {
 const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 1,
-        borderColor: 'black',
-        flexDirection: 'column',
-        height: 300,
-        alignItems: 'center',
-        justifyContent: 'center', 
+        flexDirection: 'row',
+        height: 120,
+        // alignItems: 'center',
+        justifyContent: 'space-between', 
 
     },
-    textOneStyle: {
-        borderWidth: 1,
-        borderColor: 'red',
-        flex: 4,
-        alignSelf: 'flex-end',
-        // position: 'absolute',
-        // bottom: 0
-        ...StyleSheet.absoluteFillObject
+    boxOneStyle: {
+        height:60,
+        width: 60,
+        backgroundColor: 'red',
+    },
+    boxTwoStyle: {
+        height:60,
+        width: 60,
+        backgroundColor: 'green',
+        alignSelf: 'flex-end'
         // marginHorizontal: 20,
         // marginVertical: 20
 
     },
-    textTwoStyle: {
-        borderWidth: 1,
-        borderColor: 'red',
-        flex: 4,
-        alignSelf: 'flex-start',
-        left: 10
-
-        // marginHorizontal: 20,
-        // marginVertical: 20
-
-    },
-    textThreeStyle: {
-        borderWidth: 1,
-        borderColor: 'red',
-        // marginHorizontal: 20,
-        // marginVertical: 20
-        flex: 2
+    boxThreeStyle: {
+        height:60,
+        width: 60,
+        backgroundColor: 'blue',
+        right: 0
     }
 });
 
