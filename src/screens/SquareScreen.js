49a8f2c44c98712570d8ed95cf0dd213791aa34a
nguyenhung <hunghung0309@gmail.com>
2021-02-20 10:27:11 +0700

    58. Restoring Validation Update

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -6,16 +6,16 @@ const COLOR_INCREMENT = 20;
 
 const SquareScreen = () => {
   const reducer = (state, action) => {
-    switch (action.colorToChange) {
-      case "red":
+    switch (action.type) {
+      case "change_red":
         return state.red + action.amount > 255 || state.red + action.amount < 0
         ? state
         : { ...state, red: state.red + action.amount };
-      case "green":
+      case "change_green":
         return state.green + action.amount > 255 || state.green + action.amount < 0
         ? state
         : { ...state, green: state.green + action.amount };
-      case "blue":
+      case "change_blue":
         return state.blue + action.amount > 255 || state.blue + action.amount < 0
         ? state
         : { ...state, blue: state.blue + action.amount };
@@ -30,13 +30,13 @@ const SquareScreen = () => {
       <ColorCounter
         onIncrease={() =>
           dispatch({
-            colorToChange: "red",
+            type: "change_red",
             amount: COLOR_INCREMENT,
           })
         }
         onDecrease={() =>
           dispatch({
-            colorToChange: "red",
+            type: "change_red",
             amount: -1 * COLOR_INCREMENT,
           })
         }
@@ -45,13 +45,13 @@ const SquareScreen = () => {
       <ColorCounter
         onIncrease={() =>
           dispatch({
-            colorToChange: "blue",
+            type: "change_blue",
             amount: COLOR_INCREMENT,
           })
         }
         onDecrease={() =>
           dispatch({
-            colorToChange: "blue",
+            type: "change_blue",
             amount: -1 * COLOR_INCREMENT,
           })
         }
@@ -60,13 +60,13 @@ const SquareScreen = () => {
       <ColorCounter
         onIncrease={() =>
           dispatch({
-            colorToChange: "green",
+            type: "change_green",
             amount: COLOR_INCREMENT,
           })
         }
         onDecrease={() =>
           dispatch({
-            colorToChange: "green",
+            type: "change_green",
             amount: -1 * COLOR_INCREMENT,
           })
         }
