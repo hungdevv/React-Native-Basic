edf1a622b6c97383b81d0a823ec735e69736142c
nguyenhung <hunghung0309@gmail.com>
2021-02-20 10:53:35 +0700

    Exercise Solution

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,23 +1,37 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import {View, StyleSheet, Text, Button} from 'react-native';
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'increment':
+            return {...state, count: state.count + action.payload };
+         case 'decrement':
+            return {...state, count: state.count - action.payload };
+        default:
+            return state;
+    }
+};
+
 const CounterScreen = () => {
+    // state: {count: number}
+    // action: {type: 'increment' || 'decrement', payload: 1}
+    
+                
+const [state, dispatch] = useReducer(reducer, { count: 0 })
 
-    const [counter, setCounter] = useState(0);
-
-    return <View>
-        <Button 
-            title="Increase" 
-            onPress={() => {
-            setCounter(counter + 1)
-            }}/>
-        <Button 
-            title="Decrease" 
-            onPress={()=> 
-            setCounter(counter-1)
-            }/>
-        <Text>Counter Count: {counter}</Text>
-    </View>
+return <View>
+    <Button 
+        title="Increase" 
+        onPress={() => {
+            dispatch({type: 'increment', payload: 2})
+        }}/>
+    <Button 
+        title="Decrease" 
+        onPress={()=> 
+            dispatch({type: 'decrement', payload: 1})
+        }/>
+    <Text>Counter Count: {state.count}</Text>
+</View>
 };
 
 const styles = StyleSheet.create({
